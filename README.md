80c98c48bcc9a6625fccc2a687a175ea31633d98
Lijun Xue <xljroy@gmail.com>
2019-06-07 16:45:49 +0800

    New version drafts

--- README.md
+++ README.md
@@ -2,10 +2,30 @@
 ---
 ![power_armor](https://raw.githubusercontent.com/royxue/Power-Armor/master/misc/power_armor.png)
 
-### Personal Mac Development Environment Setup Configuration
+## Personal Mac Dev Env Setup Configuration
 
-#### Some Downloads links
-1. [iterm 2](https://www.iterm2.com/)
-2. [Tower](https://www.git-tower.com/)
+This Repo is aimed for quick env setup
+
+### Manually Download
+* Quick Entry: [Alfred 4](https://www.alfredapp.com/)
+* Terminal: [iterm 2](https://www.iterm2.com/)
+* Git Client: [Tower](https://www.git-tower.com/)
+* Keymappings: [Karabiner-Elements](https://github.com/tekezo/Karabiner-Elements)
+* Automation: [Keyboard Maestro](https://www.keyboardmaestro.com/main/)
+* Editor: [Code](https://code.visualstudio.com/)
+* [Docker](https://hub.docker.com/editions/community/docker-ce-desktop-mac)
+
+### Apps from App Store
+* Bear
+* Things 3
+* Fantastical
+* Mindnode
+* BetterSnapTool
+* Amphetamine
+* Popclip
+
+### Best Practice with Todos
+* Install things 3 from app store
+* Load this [checklist]()
+
 3. [tmux config](https://github.com/gpakosz/.tmux)
-4. [Karabiner-Elements](https://github.com/tekezo/Karabiner-Elements)
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,36 @@
-# Install brew
+# Install brew for macos package management
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Brew
+# Brew the basic components
 brew update
 
-brew install thefuck
-brew install antigen
-brew install the_silver_searcher
-brew install nyancat
+# Update git version
+brew install git
 
+# Oh my zsh setup
+brew install zsh zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Fonts setup
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+
+# Tmux setup
 brew install tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf ~/.tmux/.tmux.conf
 
+# Spacemacs
 brew tap d12frosted/emacs-plus
 brew install emacs-plus
+brew linkapps emacs-plus
 
-# Create symbolink
+# Useful tools
+brew install thefuck the_silver_searcher tree antigen
+
+# For fun XD
+brew install nyancat
+
+# Create symbolink for Personal configuration
 ln -s ./ohmyzsh/.zshrc ~/.zshrc
 ln -s ./spacemacs/.spacemacs ~/.spacemacs
